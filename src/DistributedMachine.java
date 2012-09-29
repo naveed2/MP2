80f2596b87f3e9050fd9278ae9e66b4a18a417a8
conanhjj <conanhjj@gmail.com>
2012-09-29 16:47:32 -0500

    add source directory

---
+++ src/DistributedMachine.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: wind
+ * Date: 9/29/12
+ * Time: 4:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DistributedMachine {
+}
