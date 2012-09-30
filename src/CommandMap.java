14cea8f69744d2a438e2a9be4ba86f0f1e73829f
conanhjj <conanhjj@gmail.com>
2012-09-29 21:27:30 -0500

    still add some base code

---
+++ src/CommandMap.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * map command string -> function name
+ * Singleton
+ */
+public class CommandMap {
+
+    private static CommandMap instance = new CommandMap();
+    private Map<String, String> stringToFuncName;
+
+    private static final String CMD_START_CONTACT_SERVER = "start contact server";
+    private static final String FUNC_START_CONTACT_SERVER = "startContactServer";
+
+    private CommandMap() {
+
+    }
+
+    public static CommandMap getInstance() {
+        return instance;
+    }
+
+    public synchronized CommandMap initialize() {
+        instance = new CommandMap();
+        stringToFuncName = new HashMap<String, String>();
+
+        stringToFuncName.put(CMD_START_CONTACT_SERVER, FUNC_START_CONTACT_SERVER);
+
+        return this;
+    }
+
+    public synchronized String findCommand(String cmd) {
+        return stringToFuncName.get(cmd);
+    }
+
+}
--- src/DistributedMachine.java
+++ src/DistributedMachine.java
@@ -1,6 +1,8 @@
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class DistributedMachine {
@@ -9,24 +11,47 @@ public class DistributedMachine {
     private static UDPServer server;
     private static Logger logger = Logger.getLogger(DistributedMachine.class);
 
+    private static DistributedMachine instance;
+
     public static void main(String[] args) {
         log4jConfigure();
-        work();
+        try{
+            work();
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 
     private static void log4jConfigure() {
         PropertyConfigurator.configure("log4j.properties");
     }
 
-    private static void work() {
-        printWelcomeMessage();
+    private static final String CMD_START_AS_CONTACT = "start contact server";
 
-        //TODO:
-//        server = new UDPServer();
+    private static void work() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        printWelcomeMessage();
+        CommandMap commandMap = CommandMap.getInstance().initialize();
 
         while(true) {
             String cmd = inputCommand();
+            String funcName;
+
+            funcName = commandMap.findCommand(cmd);
+
+            if(funcName == null) {
+                wrongCommand();
+            } else {
+                Method method = DistributedMachine.class.getDeclaredMethod(funcName);
+                method.setAccessible(true);
+                method.invoke(null);    //static
+                method.setAccessible(false);
+            }
         }
+    }
+
+    private static void wrongCommand() {
+        System.out.println("Wrong command, please type 'help' for more information");
     }
 
     private static String inputCommand() {
@@ -34,12 +59,19 @@ public class DistributedMachine {
         return in.nextLine();
     }
 
-    private static void inputPortNumber() {
-
+    private static int inputPortNumber() {
+        System.out.print("Input the port: ");
+        //TODO: not finished yet
+        return 0;
     }
 
     private static void printWelcomeMessage() {
         System.out.println("Welcome to the fictitious Group-R-Us Inc.!");
         System.out.println("Author: Muhammad Naveed, Junjie Hu");
+    }
+
+    private static void startContactServer() {
+
+//        server = new UDPServer()
     }
 }
